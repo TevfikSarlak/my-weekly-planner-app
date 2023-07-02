d4c8c20662b975a78d6d3ca339b02a4e1bbdbe89
Tevfik <tevfiksarlak@gmail.com>
2023-07-02 13:52:57 +0300

    files structure changes

---
+++ src/components/calendar/DateDay.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Component for arranging Date and Day of month
+export default function DateDay({date, day}) {
+    return(
+        <div className="flex flex-row justify-between text-slate-500  border-b-2
+                      border-slate-500 font-semibold">
+            <div>
+                {date}
+            </div>
+            <div className="text-amber-600">
+                {day}
+            </div>    
+        </div>
+    )
+}
